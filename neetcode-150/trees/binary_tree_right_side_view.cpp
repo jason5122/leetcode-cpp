415f77560bebd3a978ccd4eae2e2545b311b9e68
Jason Han <jason.han@columbia.edu>
2024-09-15 07:48:05 -0400

    Add slightly better solution for 199.

--- neetcode-150/trees/binary_tree_right_side_view.cpp
+++ neetcode-150/trees/binary_tree_right_side_view.cpp
@@ -1,3 +1,8 @@
+/*
+n == # of nodes
+runtime: O(n)
+space: O(n)
+*/
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
@@ -15,6 +20,37 @@ public:
                 if (!chose_node_from_layer) {
                     ans.push_back(node->val);
                     chose_node_from_layer = true;
+                }
+
+                if (node->right) q.push(node->right);
+                if (node->left) q.push(node->left);
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+USED SOLUTION
+n == # of nodes
+runtime: O(n)
+space: O(n)
+*/
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+
+        vector<int> ans;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto node = q.front();
+                q.pop();
+
+                if (i == 0) {
+                    ans.push_back(node->val);
                 }
 
                 if (node->right) q.push(node->right);

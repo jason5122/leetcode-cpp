ac67f2212ab085a2164c029948d5c8cfcbd39f54
Jason Han <jason.han@columbia.edu>
2024-09-15 07:21:44 -0400

    Add iterative solution for 1448.

--- neetcode-150/trees/count_good_nodes_in_binary_tree.cpp
+++ neetcode-150/trees/count_good_nodes_in_binary_tree.cpp
@@ -19,3 +19,36 @@ private:
         return helper(root->left, new_max) + helper(root->right, new_max) + is_good;
     }
 };
+
+/*
+n == # of nodes
+runtime: O(n)
+space: O(n)
+*/
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, root->val});
+
+        int ans = 0;
+        while (!q.empty()) {
+            auto [node, curr_max] = q.front();
+            q.pop();
+
+            int val = node->val;
+            if (val >= curr_max) {
+                ans++;
+            }
+
+            int new_max = max(val, curr_max);
+            if (node->left) {
+                q.push({node->left, new_max});
+            }
+            if (node->right) {
+                q.push({node->right, new_max});
+            }
+        }
+        return ans;
+    }
+};

a26afa6954fe70434ea0cff5f42751d757104619
Jason Han <jason.han@columbia.edu>
2024-10-13 02:32:09 -0400

    Add optimized version of 1010.

--- no-debugging/pairs_of_songs_with_total_durations_divisible_by_60.cpp
+++ no-debugging/pairs_of_songs_with_total_durations_divisible_by_60.cpp
@@ -20,3 +20,25 @@ public:
         return ans;
     }
 };
+
+/*
+USED SOLUTION
+n == time.size()
+runtime: O(n)
+space: O(n)
+*/
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        vector<int> complement(60, 0);
+
+        int ans = 0;
+        for (int t : time) {
+            int mod = t % 60;
+
+            ans += complement[mod];
+            complement[(60 - mod) % 60] += 1;
+        }
+        return ans;
+    }
+};

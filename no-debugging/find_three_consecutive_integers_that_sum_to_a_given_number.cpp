ab2a42e0e8b792af3eddad77377ad695787e9fd8
Jason Han <jason.han@columbia.edu>
2024-10-13 02:45:49 -0400

    Add 2177. Find Three Consecutive Integers That Sum to a Given Number

---
+++ no-debugging/find_three_consecutive_integers_that_sum_to_a_given_number.cpp
@@ -0,0 +1,13 @@
+/*
+runtime: O(1)
+space: O(1)
+*/
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        if (num % 3 != 0) return {};
+
+        long long mid = num / 3;
+        return {mid - 1, mid, mid + 1};
+    }
+};

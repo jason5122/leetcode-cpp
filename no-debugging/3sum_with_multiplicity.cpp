5711abfeb3f09fbded992c03c34a638aa897295a
Jason Han <jason.han@columbia.edu>
2024-12-10 08:01:17 -0500

    Add 923. 3Sum With Multiplicity

---
+++ no-debugging/3sum_with_multiplicity.cpp
@@ -0,0 +1,40 @@
+/*
+arr = [1,1,2,2,3,3,4,4,5,5], target = 8
+
+arr = [1,1,2,2,2,2], target = 5
+
+Two sum:
+[1,2,2,2,2], new_target = 4
+
+[1,2,2,2,2]
+*/
+
+constexpr long kMod = 1e9 + 7;
+
+/*
+n == arr.size()
+runtime: O(n^2)
+space: O(n)
+*/
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n = arr.size();
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            int new_target = target - arr[i];
+            unordered_map<int, long> hmap;
+            for (int j = 0; j < n; j++) {
+                if (j == i) continue;
+
+                int complement = new_target - arr[j];
+                if (hmap.count(complement)) {
+                    ans += hmap[complement];
+                }
+                hmap[arr[j]]++;
+            }
+        }
+        return (ans / 3) % kMod;
+    }
+};

65d1cc540464d5c4f23a1fb4bf275f3bc16753ba
Jason Han <jason.han@columbia.edu>
2024-12-18 05:47:31 -0500

    Add math approach to 202.

--- neetcode/math-and-geometry/happy_number.cpp
+++ neetcode/math-and-geometry/happy_number.cpp
@@ -1,8 +1,10 @@
 /*
 NOT MATH
-n == # of numbers in happy number chain
-runtime: O(n)
+runtime: O(log10(n))
 space: O(1)
+
+The number of digits in a number is given by log10(n).
+The cycle length is a constant.
 */
 class Solution {
 public:
@@ -13,6 +15,37 @@ public:
             slow = next(slow);
             fast = next(next(fast));
             if (slow == fast) return false;
+        }
+        return true;
+    }
+
+private:
+    int next(int n) {
+        int result = 0;
+        while (n) {
+            int digit = n % 10;
+            n /= 10;
+            result += digit * digit;
+        }
+        return result;
+    }
+};
+
+/*
+USED SOLUTION
+runtime: O(log10(n))
+space: O(1)
+
+The number of digits in a number is given by log10(n).
+The cycle length is a constant.
+*/
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> cycle_members = {4, 16, 37, 58, 89, 145, 42, 20};
+        while (n != 1) {
+            n = next(n);
+            if (cycle_members.count(n)) return false;
         }
         return true;
     }

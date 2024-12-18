8b7cf43f40c898095729d26f156d0c36f4441ef0
Jason Han <jason.han@columbia.edu>
2024-12-18 06:04:54 -0500

    Add fancy bit manipulation solution to 191.

--- neetcode/bit-manipulation/number_of_1_bits.cpp
+++ neetcode/bit-manipulation/number_of_1_bits.cpp
@@ -1,6 +1,8 @@
 /*
-runtime: O(lg(n))
+runtime: O(1)
 space: O(1)
+
+There are 32 bits at maximum in an int, which gives this a O(1) runtime complexity.
 */
 class Solution {
 public:
@@ -9,6 +11,26 @@ public:
         while (n) {
             ans += n & 1;
             n >>= 1;
+        }
+        return ans;
+    }
+};
+
+/*
+USED SOLUTION
+runtime: O(1)
+space: O(1)
+
+There are 32 bits at maximum in an int, which gives this a O(1) runtime complexity.
+*/
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans = 0;
+        while (n) {
+            ans++;
+            // Flip least-significant 1-bit to 0.
+            n &= n - 1;
         }
         return ans;
     }
